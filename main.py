e014c2e690c12ea16baff852fa982df63dd3c1f0
davidweber <david.charles.weber@gmail.com>
2019-02-25 18:10:34 -0600

    Added more complexity.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from random import shuffle, randrange
  
-def make_maze(w = 8, h = 4):
+maze_size = { 'x': 8, 'y': 4 }
+
+def make_maze(w = maze_size['x'], h = maze_size['y']):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
     ver = [["|  "] * w + ['|'] for _ in range(h)] + [[]]
     hor = [["+--"] * w + ['+'] for _ in range(h + 1)]
@@ -24,21 +26,60 @@ def make_maze(w = 8, h = 4):
         array.append(b)
     return array
 
-def show_me(current_position, maze):
-    current_square = maze[current_position[1] * 2 - 1][current_position[0]]
+def show_me(current_position, maze, end_location):
+    display_maze = []
+    for item in maze: display_maze.append(item) 
+
+    end_square = display_maze[end_location['y'] * 2 - 1][end_location['x'] - 1]
+    s = list(end_square)
+    s[1] = '🐁'
+    display_maze[end_location['y'] * 2 - 1][end_location['x'] - 1] = "".join(s)
+
+    current_square = display_maze[current_position['y'] * 2 - 1][current_position['x'] - 1]
     s = list(current_square)
     s[1] = '🐍'
-    maze[current_position[1] * 2 - 1][current_position[0]] = "".join(s)
+    display_maze[current_position['y'] * 2 - 1][current_position['x'] - 1] = "".join(s)
 
     for row in maze:
       print("".join(row))
 
+def generate_x_mark(maze_size):
+    return {'x': 8, 'y': 4}
+
+def find_finished(current_position, end_location):
+    if current_position['x'] == end_location['x'] & current_position['y'] == end_location['y']:
+        return False
+    else:
+        return True
+
+def process_move(move, current_position, maze):
+    if move == 'n':
+        current_position['y'] -= 1
+    elif move == 's':
+        current_position['y'] += 1
+    elif move == 'e':
+        current_position['x'] += 1
+    elif move == 'w':
+        current_position['x'] -= 1
+    else:
+        print("That was an illegal move, try again")
+
 def main():
-    current_position = [0,1]
+    current_position = {'x': 1, 'y': 1}
+    end_location = generate_x_mark(maze_size)
     maze = make_maze()
-    # for row in maze:
-    #   print(row)
-    show_me(current_position, maze)
+
+    while find_finished(current_position, end_location):
+        print(chr(27) + "[2J") # Clears the terminal of previous input
+
+        show_me(current_position, maze, end_location) # Show the maze
+
+        move = input("Pick a direction ('n', 's', 'e', 'w'): ")
+        if move == 'q':
+            "Thanks for playing!"
+            break
+        else:
+            process_move(move, current_position, maze)
  
 if __name__ == '__main__':
     main()
