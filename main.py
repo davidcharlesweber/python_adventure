c505694f11b1f126677f4fc4480d878030a4cc4f
Jaragoth <jaragoth86@gmail.com>
2019-03-10 18:57:19 -0500

    Ton of changes. Finished adding option checking. Added the following of a path if that is the only option. added a loose condition if you don't have any options.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from random import shuffle, randrange
 import os
 
-maze_size = {'x': 8, 'y': 4}
-
+maze_size = {'x': 12, 'y': 8}
 
 def make_maze(w=maze_size['x'], h=maze_size['y']):
     vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
@@ -54,30 +53,54 @@ def show_me(current_position, maze, end_location, outtxt):
 
 
 def generate_x_mark(maze_size):
-    return {'x': 8, 'y': 4}
+    return {'x': maze_size['x'], 'y': maze_size['y']}
 
 
-def find_finished(current_position, end_location):
+def find_finished(current_position, end_location, turns):
     if (current_position['x'] == end_location['x']) and (current_position['y'] == end_location['y']):
+        print("You Win! Took you: {} turns".format(turns))
         return False
     else:
         return True
 
 
 def valid_moves(current_position, maze):
-    return (maze[current_position['x'] + 1][current_position['y']]) + (maze[current_position['x']][current_position['y'] + 1])
+    """Function to check your current location and give a list of valid moves"""
+    valid_moves_list = []
+    open_spaces = {
+        'n': ['+  ', '|  ', '   ']
+        , 's': ['+  ', '|  ', '   ']
+        , 'e': ['   ', ' • ']
+        , 'w': ['   ', '|  ', ' • ', '|• ']}
+
+    if current_position['y'] != 1 \
+            and maze[current_position['y'] * 2 - 2][current_position['x'] - 1] in open_spaces['n'] \
+            and '#' not in maze[current_position['y'] * 2 - 3][current_position['x'] - 1]:
+        valid_moves_list.append('n')
+    if current_position['y'] != maze_size['y'] \
+            and maze[current_position['y'] * 2][current_position['x'] - 1] in open_spaces['s'] \
+            and '#' not in maze[current_position['y'] * 2 + 1][current_position['x'] - 1]:
+        valid_moves_list.append('s')
+    if current_position['x'] != maze_size['x'] \
+            and maze[current_position['y'] * 2 - 1][current_position['x']] in open_spaces['e']:
+        valid_moves_list.append('e')
+    if current_position['x'] != 1 \
+            and maze[current_position['y'] * 2 - 1][current_position['x'] - 2] in open_spaces['w'] \
+            and '|' not in maze[current_position['y'] * 2 - 1][current_position['x'] - 1]:
+        valid_moves_list.append('w')
+
+    return valid_moves_list
 
 
-def process_move(move, current_position, maze):
-    rsp = valid_moves(current_position, maze)
-
-    if move == 'n' and current_position['y'] != 1:
+def process_move(move, current_position, options):
+    rsp = ""
+    if move == 'n' and move in options:
         current_position['y'] -= 1
-    elif move == 's' and current_position['y'] != maze_size['y']:
+    elif move == 's' and move in options:
         current_position['y'] += 1
-    elif move == 'e' and current_position['x'] != maze_size['x']:
+    elif move == 'e' and move in options:
         current_position['x'] += 1
-    elif move == 'w' and current_position['x'] != 1:
+    elif move == 'w' and move in options:
         current_position['x'] -= 1
     else:
         rsp = "That was an illegal move, try again"
@@ -93,21 +116,28 @@ def clear_screen():
 def main():
     current_position = {'x': 1, 'y': 1}
     end_location = generate_x_mark(maze_size)
+
     maze = make_maze()
     txt_for_player = ""
-
-    while find_finished(current_position, end_location):
+    turns = 0
+    while find_finished(current_position, end_location, turns):
 
         show_me(current_position, maze, end_location, txt_for_player)  # Show the maze
-
-        move = input("Pick a direction ('n', 's', 'e', 'w'): ")
+        options = valid_moves(current_position, maze)
+        if len(options) == 0:
+            print("Game over. Better luck next time.")
+            break
+        elif len(options) == 1 and turns != 0:
+            move = options[0]
+        else:
+            move = input("Pick a direction ('n', 's', 'e', 'w'): ")
+            turns += 1
         if move == 'q':
             print("Thanks for playing!")
             break
         else:
-            txt_for_player = process_move(move, current_position, maze)
+            txt_for_player = process_move(move, current_position, options)
 
-    print("Squeek!")
 
 if __name__ == '__main__':
     main()
